03584bbe3681a54ff1b1713c13682e76d831c0db
Wassim-Alouini <52957875+Wassim-Alouini@users.noreply.github.com>
2020-06-09 13:15:45 +0200

    Add files via upload

---
+++ CheckPoint2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CheckPoint2 : MonoBehaviour
+{
+
+
+    
+    float sum(float var1, float var2)
+    {
+        return (var1 + var2);
+    }
+    float prod(float var1, float var2)
+    {
+        return (var1 * var2);
+    }
+    System.Nullable<float> Div(float var1,float var2)
+    {
+        if (var2 == 0)
+        {
+            Debug.Log("Cannot divide by 0");
+            return null;
+        }
+        else
+        {
+            return (var1 / var2);
+        }
+    }
+
+
+
+
+    
+
+// Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(sum(1, 2));
+        Debug.Log(prod(5, 6));
+        Debug.Log(Div(30, 5));
+        Debug.Log(Div(6, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
